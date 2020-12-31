240d3676db765881a19be74aab5bb2038a313ec2
HaoranSun1122 <ayshr@126.com>
2020-12-31 16:54:23 +0800

    problem withorder

--- src/main/java/SPM2020/SmartParking/controller/OrderController.java
+++ src/main/java/SPM2020/SmartParking/controller/OrderController.java
@@ -1,4 +1,23 @@
-package SPM2020.SmartParking.controller;
+package com.imust.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.imust.entity.Order;
+import com.imust.entity.Park;
+import com.imust.entity.Users;
+import com.imust.service.OrderService;
+import com.imust.service.ParkService;
+
+@Controller
+@RequestMapping("/order")
 public class OrderController {
-}
+    @Autowired
+    private OrderService orderService;
