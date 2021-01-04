b232a5ec0e190615649a9c4a903ff21f76983248
HaoranSun1122 <ayshr@126.com>
2021-01-04 18:19:46 +0800

    2related problems

--- src/main/java/SPM2020/SmartParking/entity/Admin.java
+++ src/main/java/SPM2020/SmartParking/entity/Admin.java
@@ -10,3 +10,26 @@ public class Admin {
     public int getId() {
         return id;
     }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Date getCreateDate() {
+        return createDate;
+    }
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+}
