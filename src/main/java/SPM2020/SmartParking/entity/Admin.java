3177fedea3c6e0db9af552f337cccacf629e18ce
HaoranSun1122 <ayshr@126.com>
2021-01-04 18:18:41 +0800

    admin classof entity"

--- src/main/java/SPM2020/SmartParking/entity/Admin.java
+++ src/main/java/SPM2020/SmartParking/entity/Admin.java
@@ -1,4 +1,12 @@
-package SPM2020.SmartParking.entity;
+package com.imust.entity;
+
+import java.util.Date;
 
 public class Admin {
-}
+    private int id;
+    private String name;
+    private String password;
+    private Date createDate;
+    public int getId() {
+        return id;
+    }
