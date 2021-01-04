a4c79039e1426bb27b8bab21af4f09224ced655a
HaoranSun1122 <ayshr@126.com>
2021-01-05 03:30:51 +0800

    finish the message entiry

--- src/main/java/SPM2020/SmartParking/entity/Message.java
+++ src/main/java/SPM2020/SmartParking/entity/Message.java
@@ -39,3 +39,32 @@ public class Message {
     public String getAdmin_name() {
         return admin_name;
     }
+    public void setAdmin_name(String admin_name) {
+        this.admin_name = admin_name;
+    }
+    public Date getCreateDate() {
+        return createDate;
+    }
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+    public Date getAnswerDate() {
+        return answerDate;
+    }
+    public void setAnswerDate(Date answerDate) {
+        this.answerDate = answerDate;
+    }
+    public int getUser_id() {
+        return user_id;
+    }
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+    public int getAdmin_id() {
+        return admin_id;
+    }
+    public void setAdmin_id(int admin_id) {
+        this.admin_id = admin_id;
+    }
+
+}
