f4b3785e48597097d7f0da9ac4eabd10b959affb
HaoranSun1122 <ayshr@126.com>
2021-01-04 22:01:03 +0800

    private class of message

--- src/main/java/SPM2020/SmartParking/entity/Message.java
+++ src/main/java/SPM2020/SmartParking/entity/Message.java
@@ -1,4 +1,20 @@
-package SPM2020.SmartParking.entity;
+package com.imust.entity;
+
+import java.util.Date;
 
 public class Message {
-}
+    private int id;
+    private String content;
+    private int user_id;
+    private String user_name;
+    private String answer;
+    private int admin_id;
+    private String admin_name;
+    private Date createDate;
+    private Date answerDate;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
