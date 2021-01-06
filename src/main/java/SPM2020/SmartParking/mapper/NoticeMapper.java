9754243541974e11522f42e0b292cd03708fdeaf
HaoranSun1122 <ayshr@126.com>
2021-01-06 17:42:17 +0800

    work on notice page

--- src/main/java/SPM2020/SmartParking/mapper/NoticeMapper.java
+++ src/main/java/SPM2020/SmartParking/mapper/NoticeMapper.java
@@ -9,3 +9,22 @@ public class Notice {
     private String content;
     private String admin_name;
     private Date createDate;
+    private int admin_id;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
