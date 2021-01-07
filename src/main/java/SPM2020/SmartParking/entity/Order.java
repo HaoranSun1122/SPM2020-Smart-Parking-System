260f16bf18d12794e6a6a5382d440ce775eb8e6b
HaoranSun1122 <ayshr@126.com>
2021-01-07 19:05:54 +0800

    work on the order

--- src/main/java/SPM2020/SmartParking/entity/Order.java
+++ src/main/java/SPM2020/SmartParking/entity/Order.java
@@ -1,4 +1,16 @@
-package SPM2020.SmartParking.entity;
+package com.imust.entity;
+
+import java.util.Date;
 
 public class Order {
-}
+    private int id;
+    private String code;
+    private String user_name;
+    private String phone;
+    private String plate_num;
+    private String park_name;
+    private double price;
+    private double total;
+    private int user_id;
+    private int park_id;
+    private int status;
