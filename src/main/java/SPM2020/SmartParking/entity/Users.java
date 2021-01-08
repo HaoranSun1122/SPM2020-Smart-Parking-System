557a081592495bcc1784a3f7c8e7df9561c6c703
HaoranSun1122 <ayshr@126.com>
2021-01-08 16:56:37 +0800

    test the admin

--- src/main/java/SPM2020/SmartParking/entity/Users.java
+++ src/main/java/SPM2020/SmartParking/entity/Users.java
@@ -1,4 +1,25 @@
-package SPM2020.SmartParking.entity;
+package com.imust.entity;
+
+import java.util.Date;
 
 public class Users {
-}
+    private int id;
+    private String name;
+    private String phone;
+    private String plate_num;
+    private String password;
+    private Date createDate;
+    private int stauts;
+    private int point;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
