b7aef0e9e18d4712bcf9790e0db36add238e7ec6
HaoranSun1122 <ayshr@126.com>
2021-01-09 17:43:25 +0800

    interceptor of aftercompletion

--- src/main/java/SPM2020/SmartParking/interceptor/LoginInterceptor.java
+++ src/main/java/SPM2020/SmartParking/interceptor/LoginInterceptor.java
@@ -1,4 +1,17 @@
-package SPM2020.SmartParking.interceptor;
+package com.imust.interceptor;
 
-public class LoginInterceptor {
-}
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LoginInterceptor implements HandlerInterceptor{
+
+    @Override
+    public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
+            throws Exception {
+        // TODO Auto-generated method stub
+
+    }
