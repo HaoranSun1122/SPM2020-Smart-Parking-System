6ab93facf316250a15f949ef2fb3d472b68e7994
HaoranSun1122 <ayshr@126.com>
2021-01-11 07:09:12 +0800

    add the information

--- src/main/java/SPM2020/SmartParking/mapper/AdminMapper.java
+++ src/main/java/SPM2020/SmartParking/mapper/AdminMapper.java
@@ -1,4 +1,18 @@
-package SPM2020.SmartParking.mapper;
+package com.imust.mapper;
 
-public class AdminMapper {
-}
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.imust.entity.Admin;
+@Mapper
+public interface AdminMapper {
+
+    //添加信息
+    @Insert("insert into ADMIN(name,password,createDate) values(#{name},#{password},SYSDATE())")
+    public void insert(Admin admin);
