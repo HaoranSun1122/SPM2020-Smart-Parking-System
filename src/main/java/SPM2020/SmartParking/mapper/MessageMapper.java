d498b55e3078047cbad1477b7291fc17c09e16a4
HaoranSun1122 <ayshr@126.com>
2021-01-13 07:41:02 +0800

    message mapper

--- src/main/java/SPM2020/SmartParking/mapper/MessageMapper.java
+++ src/main/java/SPM2020/SmartParking/mapper/MessageMapper.java
@@ -1,4 +1,18 @@
-package SPM2020.SmartParking.mapper;
+package com.imust.mapper;
 
-public class MessageMapper {
-}
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.imust.entity.Message;
+@Mapper
+public interface MessageMapper {
+
+    //添加信息
+    @Insert("insert into Message(content,createDate,user_id,user_name) values(#{content},SYSDATE(),#{user_id},#{user_name})")
+    public void insertMessage(Message message);
