a8672761ff27b9f8ea0afbd010a812e55b24ca2f
HaoranSun1122 <ayshr@126.com>
2021-01-19 16:55:58 +0800

    read from mysql

--- src/main/java/SPM2020/SmartParking/mapper/OrderMapper.java
+++ src/main/java/SPM2020/SmartParking/mapper/OrderMapper.java
@@ -1,4 +1,19 @@
-package SPM2020.SmartParking.mapper;
+package com.imust.mapper;
 
-public class OrderMapper {
-}
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.imust.entity.Order;
+@Mapper
+public interface OrderMapper {
+
+
+    @Select("select o.status,u.plate_num,c.price,o.code,o.createDate,o.id,o.total,u.name user_name,u.phone,c.name park_name from Orders o,User u,Park c where o.user_id=u.id and o.park_id=c.id")
+    List<Order> findAllOrder();
+	
