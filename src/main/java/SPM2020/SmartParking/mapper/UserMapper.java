746be013a7fda94262fcda3c7ca92fb367cbde74
HaoranSun1122 <ayshr@126.com>
2021-01-27 17:06:54 +0800

    delete from user

--- src/main/java/SPM2020/SmartParking/mapper/UserMapper.java
+++ src/main/java/SPM2020/SmartParking/mapper/UserMapper.java
@@ -15,3 +15,7 @@ public interface UserMapper {
     //添加信息
     @Insert("insert into User(name,phone,plate_num,password,createDate,stauts) values(#{name},#{phone},#{plate_num},#{password},SYSDATE(),0)")
     public void insertUsers(Users users);
+
+    //删除信息
+    @Delete("delete from User where id=#{id}")
+    public void deleteUserById(int id);
