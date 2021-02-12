05925e2444d8e68c452c8e81ce20de02ce1cd240
HaoranSun1122 <ayshr@126.com>
2021-02-12 18:01:37 +0800

    admin

--- src/main/java/SPM2020/SmartParking/service/AdminService.java
+++ src/main/java/SPM2020/SmartParking/service/AdminService.java
@@ -1,4 +1,23 @@
-package SPM2020.SmartParking.service;
+package com.imust.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.imust.entity.Admin;
+import com.imust.mapper.AdminMapper;
+@Service
 public class AdminService {
-}
+    @Autowired
+    private AdminMapper adminMapper;
+
+    //登陆
+    public Admin login(Admin admin) {
+        List<Admin> list = adminMapper.login(admin);
+        if(list.size()>0) {
+            admin = list.get(0);
+            return admin;
+        }
+        return null;
+    }
