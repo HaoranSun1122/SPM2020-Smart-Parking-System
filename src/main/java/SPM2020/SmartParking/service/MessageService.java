8b7dfcf69abf774fb91e318fee722b419e832d85
HaoranSun1122 <ayshr@126.com>
2021-03-17 07:34:59 +0800

    finish this part

--- src/main/java/SPM2020/SmartParking/service/MessageService.java
+++ src/main/java/SPM2020/SmartParking/service/MessageService.java
@@ -14,13 +14,50 @@ public class MessageService {
     @Autowired
     private MessageMapper messageMapper;
 
-//通过id修改状态
-public boolean updateMessage(Message message) {
-    try {
-        messageMapper.updateMessage(message);
-        return true;
-    } catch (Exception e) {
-        // TODO: handle exception
-        return false;
+    //通过id修改状态
+    public boolean updateMessage(Message message) {
+        try {
+            messageMapper.updateMessage(message);
+            return true;
+        } catch (Exception e) {
+            // TODO: handle exception
+            return false;
+        }
+    }
+
+    //模糊查询
+    public List<Message> getByContent(String tmpContent) {
+        String content = "%"+tmpContent+"%";
+        return messageMapper.findByContent(content);
+    }
+    //获取全部
+    public List<Message> getAll() {
+        // TODO Auto-generated method stub
+        return messageMapper.findAllMessage();
+    }
+    public List<Message> getMyMessage(int id) {
+        // TODO Auto-generated method stub
+        return messageMapper.findMyMessage(id);
+    }
+    //删除
+    public boolean delMessage(int id) {
+        try {
+            messageMapper.deleteMessage(id);
+            return true;
+        } catch (Exception e) {
+            // TODO: handle exception
+            return false;
+        }
+    }
+
+    //添加
+    public boolean addMessage(Message message) {
+        try {
+            messageMapper.insertMessage(message);
+            return true;
+        } catch (Exception e) {
+            // TODO: handle exception
+            return false;
+        }
     }
 }
